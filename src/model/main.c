7d1097b65ca24b06173ec170a6db671d02c3273c
Leaf xu <1254551981@qq.com>
2019-11-22 21:12:45 +0800

    1. C scope 2

---
+++ src/model/main.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+ > File Name: main.c
+ > Author: Leafxu
+ > Created Time: 2019年11月22日 20:14:56
+ ************************************************************************/
+
+#include<stdio.h>
+int zm = 120;
+void printVal1();
+void printVal2();
+int main(int argc, char** agrv) {
+	int zm=20;
+	printVal1();
+	printVal1();
+	printVal2();
+	printVal2();
+	printf("zm: %d\n", zm);
+	return 0;
+}
